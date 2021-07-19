7cacbd0282df353ff8c8526a43cff3cd3472da71
Santhosh Raj <51951778+tsanthoshraj18@users.noreply.github.com>
2021-07-19 20:38:52 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,45 +1,22 @@
-﻿using AForge.Video.FFMPEG;
-using System;
-using System.Drawing;
-using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
-namespace TimeLapseCreator
+namespace Image_To_Video_Converter
 {
-    class Program
+    static class Program
     {
-        //Enter the Path here
-        const string basePath = @"E:\Pic\NIFTY-22-JUL-2021\";
-
-        static void Main(string[] args)
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
         {
-
-
-            using (var videoWriter = new VideoFileWriter())
-            {
-                //video path + and video format, Image Width , Hieght , Bitdepth
-                videoWriter.Open(basePath + "Output.avi", 1536, 742, 12, VideoCodec.MPEG4, 1000000);
-
-                //Enter the Image format 
-                var image = Directory.GetFiles(basePath, "*.png");
-
-                foreach (var images in image)
-
-
-                {
-
-                    using (Bitmap bitImage = Bitmap.FromFile(images) as Bitmap)
-                    {
-                        videoWriter.WriteVideoFrame(bitImage);
-                    }
-
-                }
-                videoWriter.Close();
-
-
-            }
-            //Console.WriteLine("Process Completed Sucessfully...");
-
-
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new ImageToVideoConverter());
         }
     }
 }
