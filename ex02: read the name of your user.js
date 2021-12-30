cc433efa1dc4f747b3824d7cf96326fa4238a6aa
Santhosh Raj <51951778+tsanthoshraj18@users.noreply.github.com>
2021-12-30 10:02:44 +0530

    Create ex02: read the name of your user.js

---
+++ ex02: read the name of your user.js
@@ -0,0 +1,4 @@
+//By using readlineSync get input and print it
+var readlinesync = require('readline-sync');//importing readline sync and using it
+var input = readlinesync.question("Your name: ");//Geting input by using readline sync
+console.log("Hello "+ input)//consle the output
