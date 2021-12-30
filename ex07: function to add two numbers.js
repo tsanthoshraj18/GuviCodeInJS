559205265ac60972378583f6262538e638900e56
Santhosh Raj <51951778+tsanthoshraj18@users.noreply.github.com>
2021-12-30 11:44:10 +0530

    Create ex07: function to add two numbers.js

---
+++ ex07: function to add two numbers.js
@@ -0,0 +1,10 @@
+function add(numOne,numTwo){
+  const ans = numOne+numTwo
+  console.log(ans)
+}
+const first = parseInt(prompt('Enter the first number '));
+const second = parseInt(prompt('Enter the second number '));
+// var first = readlinesync.question("First number is:");
+// var second = readlinesync.question("Second question is: ");
+
+add(first,second)
